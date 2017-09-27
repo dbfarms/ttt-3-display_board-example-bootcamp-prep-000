d88ac55e20ef817d7ec754cdc3239ccb602a435a
Matthew Hunger <matt.hunger@gmail.com>
2017-09-27 10:23:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board 
+  line_one = "   |   |   "
+  line_two = "-----------"
+  line_three = "   |   |   "
+  line_four = "-----------"
+  line_five = "   |   |   "
+  puts 
+end
